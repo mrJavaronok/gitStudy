0bbb2fdab90e10dcdd297885ed79bf473b013243
mrJavaronok <96792521+mrJavaronok@users.noreply.github.com>
2021-12-30 23:47:19 +0300

    information, how insert value in line

---
+++ insertValueInLine.py
@@ -0,0 +1,21 @@
+address = input('Please, Enter your IP: ')
+mac = input('Please, Enter your MAC: ').upper()
+doman = input('Please, Enter your Damain: ').upper()
+text = '''IP address: {}
+MAC address: {}
+Domane name: {}'''.format(address, mac, doman)
+print(text)
+
+'''
+Output:
+Please, Enter your IP: 10.128.52.62
+Please, Enter your MAC: ab-00-ca-46-gb
+Please, Enter your Damain: mail.ru
+IP address: 10.128.52.62    
+MAC address: AB-00-CA-46-GB 
+Domane name: mail.ru    
+
+///
+text = 'IP address: {}\nMAC address: {}\nDomane name: {}'.format(address, mac, doman)
+print(text)
+'''
